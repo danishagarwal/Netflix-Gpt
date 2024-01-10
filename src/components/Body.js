90d64b5eb69122e16a3fa6bd46546837886b2de7
Danish Agarwal <danishagarwal9@gmail.com>
2024-01-10 20:31:14 +0530

    cleaning

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,10 @@
-import React, { useEffect } from "react";
-import { createBrowserRouter, useNavigate } from "react-router-dom";
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
 import Login from "./Login";
 import Browse from "./Browse";
 import { RouterProvider } from "react-router-dom";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
-import { auth } from "../utils/firebase";
-import { useDispatch } from "react-redux";
-import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
-  const dispatch = useDispatch();
-
   const appRouter = createBrowserRouter([
     {
       path: "/",
